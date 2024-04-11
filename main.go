df9830d3242defb5463462c9c497af01ccc35b55
Harshit Tomar <harshitt345@gmail.com>
2024-04-11 15:32:39 +0530

    real flow

--- main.go
+++ main.go
@@ -4,16 +4,17 @@ import (
 	"fmt"
 	"image"
 	"image/color"
-	_ "image/png"
 	"log"
-	"os"
 
-	// "time"
+	// _ "image/png"
+	// "os"
+
+	"time"
 
 	"github.com/kbinani/screenshot"
 )
 
-func getAvgGray(img image.Image) uint {
+func getPercentLightness(img image.Image) uint8 {
 	imgSize := img.Bounds().Size()
 
 	var sum uint
@@ -24,35 +25,84 @@ func getAvgGray(img image.Image) uint {
 	}
 	sum = sum / uint(imgSize.X*imgSize.Y)
 
-	return sum
+	var percentage uint8 = uint8(100 * sum / 255)
+
+	return percentage
 }
 
 func main() {
-	inputFile, err := os.Open("test_img/test_img_copy.png")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer inputFile.Close()
-
-	img, _, err := image.Decode(inputFile)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	var percentage uint = 100 * getAvgGray(img) / 255
-
-	fmt.Printf("Brightness: %d%%\n", percentage)
 
 	n := screenshot.NumActiveDisplays()
+	var normalBrightness int8 = 40
+	var maxDeviation int8 = 20
+	var threshold int8 = 5
+	var prevLightness []uint8
 
+	fmt.Printf("Detected %d displays connected\n", n)
 	for i := 0; i < n; i++ {
-		bounds := screenshot.GetDisplayBounds(i)
-
-		img, err := screenshot.CaptureRect(bounds)
+		img, err := screenshot.CaptureDisplay(i)
 		if err != nil {
-			panic(err)
+			log.Fatalln(err)
 		}
-
-		fmt.Printf("\nDisplay #%d\nLightness: %d%%\n\n", i, 100*getAvgGray(img)/255)
+		prevLightness[i] = getPercentLightness(img)
+		fmt.Printf("Display #%d\nLightness: %d%%\n\n", i, prevLightness[i])
 	}
+
+	for range time.Tick(time.Second * 2) {
+		go func() {
+			unchanged := true
+			k := screenshot.NumActiveDisplays()
+			if n != k {
+				unchanged = false
+				n = k
+			}
+
+			var lightness []uint8
+
+			for i := 0; i < n; i++ {
+				img, err := screenshot.CaptureDisplay(i)
+				if err != nil {
+					log.Fatalln(err)
+				}
+				lig := getPercentLightness(img)
+				if unchanged {
+					if prevLightness[i] != lig {
+						unchanged = false
+						lightness = prevLightness
+						lightness[i] = lig
+					}
+				} else {
+					lightness[i] = lig
+				}
+			}
+
+			if !(unchanged) {
+				for i := 0; i < n; i++ {
+					var delta int8 = int8(lightness[i]) - int8(prevLightness[i])
+					if delta > threshold || delta < -threshold {
+						var brightness int8 = normalBrightness + (int8(lightness[1])-50)*maxDeviation/100
+						fmt.Printf("Changed Brightness for Screen #%d\nLightness: %d\nNewBrightness: %dlightness\n\n", i, lightness[i], brightness)
+					}
+				}
+				prevLightness = lightness
+			}
+
+		}()
+	}
+
 }
+
+// inputFile, err := os.Open("test_img/test_img_copy.png")
+// if err != nil {
+// 	log.Fatalln(err)
+// }
+// defer inputFile.Close()
+
+// img, _, err := image.Decode(inputFile)
+// if err != nil {
+// 	log.Fatalln(err)
+// }
+
+// var percentage uint = getPercentLightness(img)
+
+// fmt.Printf("Brightness: %d%%\n", percentage)
